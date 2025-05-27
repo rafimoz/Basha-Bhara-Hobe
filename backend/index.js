d7680eb7e1c7f50a3e25ac10701d28ab9fbfc9b9
Rafi Mozumder <rafimozumder@gmail.com>
2025-05-27 20:23:35 +0600

    feat: Implement user authentication and update dependencies
    This commit introduces user authentication functionality, including login and registration pages, and updates dependencies for react-router-dom.
    
    The following changes were made:
    
    - Added Login and Signup pages to the frontend.
    - Implemented login and registration routes in the backend.
    - Updated react-router-dom to version 6.30.1.
    - Created User model in the backend.

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,15 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import adRoutes from "./routes/adRoutes.js";
-import path from 'path';
-import { fileURLToPath } from 'url';
-import connectCloudinary from './config/cloudinary.js'
-
+import path from "path";
+import { fileURLToPath } from "url";
+import connectCloudinary from "./config/cloudinary.js";
+import UserModel from "./models/User.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.resolve(__dirname, './.env') });
-
+dotenv.config({ path: path.resolve(__dirname, "./.env") });
 
 const app = express();
 app.use(cors());
@@ -20,11 +19,11 @@ app.use(express.json({ limit: "20mb" }));
 
 app.use("/api", adRoutes);
 
-// app.get('/', (req, res) => {
-//   res.send('Hello from backend API');
-// });
+app.get("/", (req, res) => {
+  res.send("Hello from backend API");
+});
 
-connectCloudinary()
+connectCloudinary();
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
@@ -32,4 +31,25 @@ mongoose
       console.log(`Server started on port ${process.env.PORT}`);
     });
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  UserModel.findOne({ email: email }).then(user => {
+    if (user) {
+      if (user.password === password) {
+        res.json(user._id);
+      } else {
+        res.json("the password is incorrect");
+      }
+    } else {
+      res.json("No record existed");
+    }
+  })
+});
+
+app.post("/register", (req, res) => {
+  UserModel.create(req.body)
+    .then((user) => res.json(user))
+    .catch((err) => res.json(err));
+});
